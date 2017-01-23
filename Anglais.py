6c1488e25b21826f321a58bcba15a11c80f4673a
Julien Palard <julien@palard.fr>
2017-01-23 19:09:38 +0100

    Try to remove duplication.

--- Anglais.py
+++ Anglais.py
@@ -1,83 +1,50 @@
+#!/usr/bin/env python3
+
 import random
 
+WORDS = tuple({"Rouge": "Red",
+               "Vert": "Green",
+               "Bleu": "Bleu",
+               "Jaune": "Jaune",
+               "Soleil": "Sun",
+               "Lundi": "Monday",
+               "Mardi": "Tuesday",
+               "Mercredi": "Wednesday",
+               "Jeudi": "Thursday",
+               "Vendredi": "Friday",
+               "Samedi": "Saturday",
+               "Dimanche": "Sunday",
+               "Demain": "Tomorrow",
+               "Hier": "Yesterday",
+               "Aujourd'hui": "Today",
+               "être": "Be",
+               "Avoir": "Have"}.items())
 
-def mot_francais(nbr_fra):
-    mots = ["Rouge","Vert","Bleu","Jaune","Soleil","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche","Demain","Hier","Aujourd'hui","être","Avoir"]
-   
+
+def main_menu():
+    print("1.  Anglais --> Français")
     print()
-    print(mots[nbr_fra])
+    print("2.  Français --> Anglais")
     print()
+    return input("Quel est ton choix :")
 
 
-def mot_anglais():
-    mots =["Red","Green","Bleu","Jaune","Sun","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Tomorrow","Yesterday","Today","Be","Have"]
-    Nbr = Random.Randint(0,3)
-    print()
-    print(mots[nbr])
-    print()
+def ask(word_to_translate, translated_word):
+    answer = input("Quel est la traduction de {} :".format(word_to_translate))
+    if answer.lower() == translated_word.lower():
+        print("Bonne réponse !")
+    else:
+        print("Non, c'est {}".format(translated_word))
 
 
-   
+def main(choice, runs=15):
+    for _ in range(runs):
+        if int(choice) == 1:
+            word_to_translate, translated_word = random.choice(WORDS)
+        else:
+            translated_word, word_to_translate = random.choice(WORDS)
+        ask(word_to_translate, translated_word)
 
-    
-print("1.  Anglais --> Français")
-print()
-print("2.  Français --> Anglais")
-print()
-choix = input("Quel est ton choix :")
-def Defilement():
-    if int(choix) == 1:
-        nbr_fra=random.randint(0,16)
-        mot_francais(nbr_fra)
-        rep=input("Quel est sa traduction :")    
-        if rep=="Red" and nbr_fra == 0:
-            print("Bonne réponse")
 
-        if rep == "Green" and nbr_fra == 1:
-            print("Bonne réponse")
-        
-        if rep =="Yellow" and nbr_fra == 3:
-            print("Bonne réponse")
-       
-        if rep == "Blue" and nbr_fra == 2:
-            print("Bonne réponse")
-        
-        if rep == "Sun" and nbr_fra == 4:
-            print("Bonne réponse")
-        
-            
-        if rep == "Monday" and nbr_fra == 5:
-            print("Bonne réponse")
-        
-        if rep == "Tuesday" and nbr_fra == 6:
-            print("Bonne réponse")
-        
-        if rep == "Wednesday" and nbr_fra == 7:
-            print("Bonne Réponse")
-        if rep == "Thursday" and nbr_fra == 8:
-            print("Bonne réponse")
-        if rep == "Friday" and nbr_fra == 9:
-            print("Bonne réponse")
-        if rep == "Saturday" and nbr_fra == 10:
-            print("Bonne réponse")
-        if rep == "Sunday" and nbr_fra ==11:
-            print("Bonne réponse")
-        if rep == "Tomorrow" and nbr_fra == 12:
-            print("Bonne réponse")
-        if rep == "Yesterday" and nbr_fra == 13:
-            print("Bonne réponse")
-        if rep == "Today" and nbr_fra == 14:
-            print("Bonne réponse")
-        if rep == "Be" and nbr_fra == 15:
-            print("Bonne réponse")
-        if rep =="Have" and nbr_fra ==16:
-            print("Bonne réponse")
-                
-        
-        
-
-i=0
-while i < 15:
-    Defilement()
-    i=i+1
-    
+if __name__ == '__main__':
+    main(main_menu())
